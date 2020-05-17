0f228a433fdc998eae4b6026baedc2178bfef967
yvnpaz <yppousa@esei.uvigo.es>
2020-05-17 11:41:14 +0200

    Correction with tests

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,28 +7,29 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    result = ''
+    result=''
     player = player.capitalize()
-    print("Player: ", player)
-    print("Ai: ", ai)
+    ai = ai.capitalize()
+    print('Player: ', player)
+    print('AI: ', ai)
     if player == ai:
-        result = "Empate!"
-    elif (player == "Papel" and ai == "Tijeras") or (player == "Tijeras" and ai == "Piedra"):
-        result = "Perdiste!"
-    elif (player == "Piedra" and ai== "Tijeras") or (player == "Papel" and ai == "Piedra"):
-        result = "Ganaste!"
+        result = 'Empate!'
+    elif (player == 'Papel' and ai == 'Tijeras') or (player == 'Tijeras' and ai == 'Piedra') or (player == 'Piedra' and ai == 'Papel'):
+        result = 'Perdiste!'
+    elif (player == 'Piedra' and ai== 'Tijeras') or (player == 'Papel' and ai == 'Piedra') or (player == 'Tijeras' and ai == 'Papel'):
+        result = 'Ganaste!'
     return result
 
 # Entry Point
 def Game():
-    player = input("Introduce an option between: Piedra, Papel or Tijeras: ")
+    player = input('Introduce una opcion entre piedra, papel or tijeras: ')
 
     # Copy array
     optionsGame = options
 
     #Value IA
     ai = random.choice(optionsGame)
-    
+
     winner = quienGana(player, ai)
 
     print(winner)
